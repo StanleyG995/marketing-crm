cd6e8457cf91b71ca058bfa03d11e4d5db0d681c
StanislawG <goral.stanislaw95@gmail.com>
2025-10-02 18:30:59 +0200

    created card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
+import type { CardProps } from '../types/Card.ts'
 
-function Card() {
-
-    type CardProps = {
-
-    }
-
-    return 
-        <div className='card'>
-            <h2 className='card__title'></h2>
+const Card: React.FC<CardProps> = ( {title, children} ) => {
+    return (
+        <div className="card">
+            <h2 className="card__title">{title}</h2>
+            <div className="card__content">{children}</div>
         </div>
-    
+    )
 }
 
 export default Card
