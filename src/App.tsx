a67ce36c86132c98c1d7b4ef7918a7e6e54834e4
StanislawG <goral.stanislaw95@gmail.com>
2025-10-02 19:06:52 +0200

    added scss colors variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-
+import Card from './components/Card'
 
 function App() {
 
-  
-
   return (
     <>
-      
+      <Card title = 'Tytuł'>
+        <p>Treść wewnątrz karty.</p>
+      </Card>
     </>
   )
 }
