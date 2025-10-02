efbd7eee857a29ec7a22674e9a995b930ed8508f
StanislawG <goral.stanislaw95@gmail.com>
2025-10-02 18:20:17 +0200

    created CardProps type

---
+++ src/components/Card.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Card() {
+
+    type CardProps = {
+
+    }
+
+    return 
+        <div className='card'>
+            <h2 className='card__title'></h2>
+        </div>
+    
+}
+
+export default Card
