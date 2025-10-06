72cc4521456ec1946beb04a75dd47a473f77bc9b
Stanlej95 <goral.stanislaw95@gmail.com>
2025-10-06 11:37:13 +0200

    installed react icons

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,9 @@ function App() {
       <Button className = 'button'  onclick = { () => console.log('Działa!') }>
         Kliknij mnie!
       </Button>
+      <Button className = 'button'  onclick = { () => console.log('Działa!') }>
+        Kliknij mnie!
+      </Button>
     </>
   )
 }
